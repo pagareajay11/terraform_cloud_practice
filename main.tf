40c280d9caa4ff8362ae08347c241a1b11eef699
Ajay Pagare <ajay.pagare@gslab.com>
2022-09-22 15:01:27 +0530

    second

--- main.tf
+++ main.tf
@@ -6,6 +6,11 @@ variable "instance_type" {
     type = string
   
 }
+provider "aws" {
+    access_key = var.access_key
+    secret_key = var.secret_key  
+}
+
 resource "aws_instance" "web" {
   ami                    = var.image_id
   instance_type          = var.instance_type
