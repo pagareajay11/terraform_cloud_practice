cce26c4f9469ddf3b46d1593dcf67c9b22068162
Ajay Pagare <ajay.pagare@gslab.com>
2022-09-22 15:14:12 +0530

    forth

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ variable "instance_type" {
     type = string
   
 }
+variable "access_key" {
+  type = string
+}
+variable "secret_key" {
+  type = string
+}
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
